677393a6460dfc28afc116247481ab2c4dc395c8
MUHAMMAD NUR ARASY <120718183+muhammadnurarasy@users.noreply.github.com>
2024-10-20 14:15:14 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
-<script>
-        const keycloak = new Keycloak({
-            url: 'http://localhost:8080/auth',
-            realm: 'myRealm',
-            clientId: 'my-client'
-        });
 
-        document.getElementById('login-btn').addEventListener('click', function () {
-            keycloak.login();
-        });
-    </script>
+const keycloak = new Keycloak({
+    url: 'http://localhost:8080/auth',
+    realm: 'myRealm',
+    clientId: 'my-client'
+});
+
+document.getElementById('login-btn').addEventListener('click', function () {
+    keycloak.login();
+});
+
